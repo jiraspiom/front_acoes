8f934bddbc5892314e3cfdc642ad7e0918dfe790
1gilmar <1gilmar@gmail.com>
2024-01-20 11:37:04 -0300

    ajustado

--- src/app/lancamento/page.tsx
+++ src/app/lancamento/page.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { formatStringToDateLocal, formatValor } from '../lib/format'
 
 type dadosTransacao = {
   id: string
@@ -34,39 +35,54 @@ export default async function Page() {
 
   // const data = await getData()
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Operacao</th>
-            <th>ativo</th>
-            <th>setor</th>
-            <th>preco</th>
-            <th>quantidade</th>
-            <th>valor Operacao</th>
-            <th>Val Total Acu</th>
-            <th>qtd Acu</th>
-            <th>media preco</th>
-          </tr>
-        </thead>
-        <tbody>
-          {result.map((item) => (
-            <tr key={item.id}>
-              <td>{item.data}</td>
-              <td>{item.operacao}</td>
-              <td>{item.ativo}</td>
-              <td>{item.setor}</td>
-              <td>{item.preco}</td>
-              <td>{item.quantidade}</td>
-              <td>{item.valOperacao}</td>
-              <td>{item.valorTotalAcumulado}</td>
-              <td>{item.quantidadeAcumulada}</td>
-              <td>{item.mediaPreco}</td>
+    <div className="max-w-fit p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="px-6 py-1 text-center">Data</th>
+              <th className="px-6 py-1 text-center">Operacao</th>
+              <th className="px-6 py-1 text-center">Ativo</th>
+              <th className="px-6 py-1 text-center">Setor</th>
+              <th className="px-6 py-1 text-center">Preço</th>
+              <th className="px-6 py-1 text-center">Qtd</th>
+              <th className="px-6 py-1 text-center">V.operacao</th>
+              <th className="px-6 py-1 text-center">V.totalAC</th>
+              <th className="px-6 py-1 text-center">qtd Ac</th>
+              <th className="px-6 py-1 text-center">M.preço</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {result.map((item) => (
+              <tr
+                key={item.id}
+                className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+              >
+                <td className="px-6 py-1">
+                  {formatStringToDateLocal(item.data)}
+                </td>
+                <td className="px-6 py-1">{item.operacao}</td>
+                <td className="px-6 py-1">{item.ativo}</td>
+                <td className="px-6 py-1">{item.setor}</td>
+                <td className="px-6 py-1 text-right">{item.preco}</td>
+                <td className="px-6 py-1 text-center">{item.quantidade}</td>
+                <td className="px-6 py-1 text-right">
+                  {formatValor(item.valOperacao)}
+                </td>
+                <td className="px-6 py-1 text-right">
+                  {formatValor(item.valorTotalAcumulado)}
+                </td>
+                <td className="px-6 py-1 text-center">
+                  {item.quantidadeAcumulada}
+                </td>
+                <td className="px-6 py-1 text-right">
+                  {formatValor(item.mediaPreco)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
---
+++ src/app/lib/format.ts
@@ -0,0 +1,18 @@
+export function formatStringToDateLocal(dataString: string) {
+  const data = new Date(dataString)
+  return data.toLocaleDateString()
+
+  //   const dia = ('0' + data.getUTCDate()).slice(-2)
+  //   const mes = ('0' + (data.getUTCMonth() + 1)).slice(-2) // Os meses em JavaScript são indexados a partir de 0
+  //   const ano = data.getUTCFullYear()
+  //   return `${dia}/${mes}/${ano}`
+}
+
+export function formatValor(valorString: string) {
+  const valorNumerico = parseFloat(valorString)
+  return valorNumerico.toLocaleString('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
+  return valorNumerico.toFixed(2)
+}
