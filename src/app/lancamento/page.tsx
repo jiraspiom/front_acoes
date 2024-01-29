bb938827c23fec76536ab63396a854d5e754ed61
1gilmar <1gilmar@gmail.com>
2024-01-29 12:13:08 -0300

    ajustado projeto

--- src/app/lancamento/page.tsx
+++ src/app/lancamento/page.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { formatStringToDateLocal, formatValor } from '../lib/format'
+import { Search, PlusCircle } from 'lucide-react'
 
 type dadosTransacao = {
   id: string
@@ -31,9 +32,37 @@ export default async function Page() {
     <div className="max-w-fit p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <div>
-          <p className="text-lg font-bold mb-2 text-gray-500 dark:text-gray-400">
+          <p className="text-lg font-bold mx-4 my-4 mb-2 text-gray-500 dark:text-gray-400">
             Valor Total Acumulado: {formatValor(totalAcumulado.toString())}
           </p>
+        </div>
+
+        <div className="p-6 max-w-4xl mx-auto space-y-4">
+          <div className="flex items-center justify-between">
+            <form className="flex items-center gap-2">
+              <input
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="ativo"
+              />
+              <input
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="setor"
+              />
+              <button
+                type="submit"
+                className=" flex py-2.5 px-5 me-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              >
+                <Search className="w-4 h-4 mr-2"></Search>
+                Filtrar
+              </button>
+            </form>
+            <button className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+              <PlusCircle className="w-4 h-4 mr-2"></PlusCircle>
+              Novo lancamento
+            </button>
+          </div>
         </div>
 
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
--- src/app/nova/page.tsx
+++ src/app/nova/page.tsx
@@ -43,24 +43,21 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <h1>criar</h1>
+    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <h1 className="max-w-sm mx-auto block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        NOVO LANÇAMENTO DE AÇÃO
+      </h1>
       <form onSubmit={handleSubmit(handleForm)} className="max-w-sm mx-auto">
-        <div>
-          <div>
-            <label>DATA: </label>
-          </div>
+        <div className="mb-4">
           <input
             type="date"
             {...register('data')}
-            className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
           {errors.data?.message && <p>{errors.data.message}</p>}
         </div>
-        <div>
-          <div>
-            <label>OPERAÇÃO: </label>
-          </div>
+
+        <div className="mb-4">
           <select
             {...register('operacao')}
             className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -71,10 +68,7 @@ export default function Home() {
           </select>
         </div>
 
-        <div>
-          <div>
-            <label>Ativo: </label>
-          </div>
+        <div className="mb-4">
           <input
             type="text"
             {...register('ativo')}
@@ -84,10 +78,7 @@ export default function Home() {
           {errors.ativo?.message && <p>{errors.ativo.message}</p>}
         </div>
 
-        <div>
-          <div>
-            <label>Setor: </label>
-          </div>
+        <div className="mb-4">
           <select
             {...register('setor')}
             className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -101,10 +92,7 @@ export default function Home() {
           </select>
         </div>
 
-        <div>
-          <div>
-            <label>Quantidade de ações: </label>
-          </div>
+        <div className="mb-4">
           <input
             type="text"
             {...register('quantidade')}
@@ -114,10 +102,7 @@ export default function Home() {
           {errors.quantidade?.message && <p>{errors.quantidade.message}</p>}
         </div>
 
-        <div>
-          <div>
-            <label>Preco: </label>
-          </div>
+        <div className="mb-4">
           <input
             type="text"
             {...register('preco')}
@@ -127,12 +112,14 @@ export default function Home() {
           {errors.preco?.message && <p>{errors.preco.message}</p>}
         </div>
 
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          enviar
-        </button>
+        <div className="mb-4">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            ENVIAR
+          </button>
+        </div>
       </form>
     </div>
   )
